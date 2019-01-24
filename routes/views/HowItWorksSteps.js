7c3ba264dea9e076b4da991e4735487128bb90d1
Ivan <ivan.rahn@gmail.com>
2019-01-24 15:12:28 +1100

    test

--- routes/views/HowItWorksSteps.js
+++
@@ -1,9 +0,0 @@
-var keystone = require('keystone');
-
-exports = module.exports = async function (req, res) {
-    
-	const steps = await keystone.list('HowItWorksSteps').model.find();
-	console.log('​steps', steps)
-    res.send(steps);
-
-}
