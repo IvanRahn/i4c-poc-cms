f5e0b581ce4d5a5b342b59fe6d6247482904896f
Ivan <ivan.rahn@gmail.com>
2019-01-24 15:04:57 +1100

    fix

--- routes/views/HowItWorksSteps.js
+++ routes/views/HowItWorksSteps.js
@@ -1,7 +1,9 @@
 var keystone = require('keystone');
 
 exports = module.exports = async function (req, res) {
-	const steps = await keystone.list('HowItWorksSteps').model.find();
-    res.send(steps);
     
+	const steps = await keystone.list('HowItWorksSteps').model.find();
+	console.log('​steps', steps)
+    res.send(steps);
+
 }
