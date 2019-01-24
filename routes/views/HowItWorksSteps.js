3f8eefeff97cfa2e390fd66fbda26411ebb8ad3a
Ivan <ivan.rahn@gmail.com>
2019-01-24 14:32:51 +1100

    change route

--- routes/views/HowItWorksSteps.js
+++ routes/views/HowItWorksSteps.js
@@ -3,4 +3,5 @@ var keystone = require('keystone');
 exports = module.exports = async function (req, res) {
 	const steps = await keystone.list('HowItWorksSteps').model.find();
     res.send(steps);
+    
 }
